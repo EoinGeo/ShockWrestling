627ec19b84361a0a9d680f8ccaf31e1bdaa0f58f
Eoin-Desktop <geogheganeoin@gmail.com>
2026-03-01 21:51:21 +0000

    End of Feb Update

--- pamphlet/app/data.ts
+++ pamphlet/app/data.ts
@@ -1,33 +1,34 @@
 // CAW-related exports (not applicable for this Strike event)
 export const topCawMoments = [
-   { Header: "Robert Goes Ace to Face - 0:16", Desc: "Rob was headed out to the ring to celebrate his title victory but Ace interrupted him to tell Rob that he couldn't beat Nicholas and that Eoin was the one who saved him. Ace announces that Nicholas has to go away for a little bit but that if Rob still holds the ShockWrestling World Championship that Robert will face the most important fight of his life. ", Rating: 4.5 },
-{ Header: "Ashlee announces who she wants to face at Mayhem 4 - 32:38", Desc: "The 2026 Women's Shock Rock Rumble winner Ashlee came out to the ring to announce who she is challenging at Mayhem IV, it is Danielle. Danielle comes down to the ring with the Women's World Championship and announces 'you're on' and then both women were attacked by a masked individual. ", Rating: 5 },
-{ Header: "Sean Payne and Patrick Cross talk it out in ring - Andre therapy session - 47:53", Desc: "Andre called Pat Cross and Sean Payne down to the ring and says he believes they are one of the very best tag teams in ShockWrestling history and asks them to talk it out. The team shakes hands and looks forward to their next title defense.", Rating: 4 },
-];
+   { Header: "Andre announcement - 0:22", Desc: "At the start of the show Andre Pinning Robert's announced two Triple Threat matches, where the winner of each of those would go on to face off in the main event of CAW. The winner of the main event will be the new #1 contender for Robert's World Championship.", Rating: 4.5 },
+{ Header: "Cameron Barker attacks Eoin - 56:43", Desc: "Cameron Barker is determined to get under Eoins skin after attacking him tonight with the rings he's been using to knock out his opponents.", Rating: 4 },
+{ Header: "Robert Ballantyne Stares Down Ziggy", Desc: "After Ziggy defeated Zacko to become the new #1 contender, the champion Robert came down to the ring to stare off against his future foe while raising the title in the air.", Rating: 5 },];
 export const topProvingGroundsMoments = [
     { Header: "It's a Madz World.", Desc: "With a little help from 'The Witch Queen' Relka, Madz defeats Tsuki Adashi to become the new Women's Proving Grounds Champion ", Rating: 4 },
 { Header: "Vigilante Zooms ", Desc: "El Vigilante Violente defeated Ziggy Danger to win his first ever singles championship here in ShockWrestling", Rating: 4 },
 { Header: "The Ruler Of Proving Grounds", Desc: "Russell Tanaka is the new Proving Grounds Champion after an elimination fatal-fourway. After the reigning champion Jacob was eliminated first, the team of Blaze & Diaz set their sights on 'The Tank' but then Blaze turned on Diaz, eliminating him. Tanaka went on to prove why he rules these grounds, emerging victorious.", Rating: 5 },
 ];
 export const topStrikeMoments = [
-    { Header: "Pure Poison Infects Strike", Desc: "It's revealed that Pat Cross is the new co-commentator on Friday Night Strike", Rating: 5 },
-    { Header: "Ace Up The Sleeve", Desc: "Ace interrupts the 2026 men's Shock Rock Rumble winner TJ Baker and congratulated him, but said TJ could be better & made him an offer to have TJ become 'his champion'", Rating: 5 },
-    { Header: "The Rebel Is Here", Desc: "TJ Bakers mystery partner was revealed to be Eoin, who came face to face with Ace as he made his way down to the ring. ", Rating: 5 },
+    { Header: "Animosity in the Arena", Desc: "Animus enters the ring and starts addressing the 2026 Shock Rock Rumble winner and Emerson Rivers, the #1 Contender for Animus' Global Championship took offense to that. They proceeded to hurl insults at each other while playing mind games ahead of their bout at Saturday Night's Crowdpleasing Event.", Rating: 5 },
+{ Header: "Supreme Ego", Desc: "Backstage we see Zach Gold complain he's not being taken seriously as the SWF World Champion and Arik Alexander takes issue. Later tonight we will see Arik vs Zach Gold with the SWF World title on the line", Rating: 4 },
+{ Header: "Even Steven", Desc: "Ahead of his SWF championship match at SWF: Supremacy, Steven attacked Zach's right hand man Ricky Star ", Rating: 4 },
 ];
 // Big Moment Images
-export const cawBigMoment = "../images/CAW160226.jpg";
+export const cawBigMoment = "../images/CAW230226.jpg";
 export const provingGroundsBigMoment = "../images/PG110226.jpg";
-export const strikeBigMoment = "../images/Strike200226.jpg";
+export const strikeBigMoment = "../images/Strike270226.jpg";
 export const interviewMoment = "../images/cameronBarker.jpg";
 // All Matches Data
 export const cawData = [
-   { Header: "Flame Shadow vs Jacob Hendry - 4:31", Desc: "Flame wins via pinfall after catching Jacob with a Flame Cutter onto a chair.", Rating: 4 },
-{ Header: "Germ Stars vs Ultimo Macho - 16:03", Desc: "Ultimo Macho gets the biggest win of his career when by rolling up Germstar for the 3 count.", Rating: 4 },
-{ Header: "Steven Chronicles vs Tyson - 24:13", Desc: "Steven gets a bit of revenge against Tyson by getting the win via count out.", Rating: 3 },
-{ Header: "Rodkip vs Hammer Bros - 3-on-1 handicap match- 37:53", Desc: "Rodkip put up a valiant effort but succumbed to the numbers advantage as the reunited Hammer Bros get the victory.", Rating: 3.5 },
-{ Header: "El Vigilante Violente and Flyboy DJ Holmes vs Velocity Express vs Caleb Blaze and Vince Diaz - 50:25", Desc: "Caleb Blaze & Vinny Diaz are the new #1 contenders for the ShockWrestling Tag Team Championships after emerging victorious in a chaotic match. ", Rating: 4 },
-{ Header: "Ziggy danger vs Zacko- 59:16", Desc: "Ziggy Danger wins via pinfall in a fast paced match. ", Rating: 3 },
-{ Header: "Eoin vs Cameron Barker- European championship - 1:05:28", Desc: "Eoin was originally scheduled to go up against Tyler Frost, however Frost was seen knocked out backstage and Cameron Barker challenged Eoin. Eoin continued his undefeated streak after winning via pinfall. ", Rating: 4 },
+   { Header: "Zacko vs Damian Cruz vs JAB - winner goes to main event - 1:14", Desc: "Zacko gets a huge win and advances to the main event of the evening. ", Rating: 4 },
+{ Header: "Ziggy Danger vs Big H vs Alex Dean - winner goes to main event - 16:46", Desc: "Ziggy got the pin on Alex Dean and will meet Zacko in the main event.", Rating: 4 },
+{ Header: "Ashlee open challenge - Nikita Campbell Answers - 24:36", Desc: "Ashlee picked up a decisive victory and then was attacked after the match by the masked figure.", Rating: 3.5 },
+{ Header: "Germ vs Ultimo Macho -  Shock wrestling TV Title - 33:30", Desc: "Germ attacked Ultimo Macho prior to the matches start. Macho elected to continue on with the match and was quickly put away with Germs Review. Germ continued to attack after he retained the SFSG TV Title. ", Rating: 2 },
+{ Header: "Filthy Flame Shadow & Tyson vs Jacob & Steven Chronicles - 36:58", Desc: "Jacob picked up the win for his team after hitting 'The Fridge' and pinning 'Filthy' Flame Shadow. After their loss, Tyson turned on Flame, repeatedly attacking him.", Rating: 3.5 },
+{ Header: "Tyler Frost vs Cameron Barker - 46:41", Desc: "Cameron Barker pulled out a win over fellow rookie Tyler Frost after nailing a punch utilizing his 2 rings. ", Rating: 3 },
+{ Header: "Eoin vs Luke 'Michael' Mercer - European championship - 56:43", Desc: "Eoins undefeated streak continues after defeating Luke Mercer, but after the match Cameron Barker rushed the ring and attacked Eoin.", Rating: 4 },
+{ Header: "Diaz and Blaze vs Payne and Poison- World Tag Team championships - 1:05:49", Desc: "Payne & Poison continue to dominate the tag division on CAW racking up another successful defense.", Rating: 4 },
+{ Header: "Zacko vs Ziggy Danger - WInner Faces Robert - 1:16:10", Desc: "Ziggy pulled off the biggest win of his career so far by winning his 2nd match of the night and will go on to challenge Robert Ballantyne for the ShockWrestling World Championship at Saturday Nights Crowdpleasing Event.", Rating: 4.5 },
 ];
 export const provingGroundsData = [
     { Header: "Ian Falcon vs Kyle Murphy - 1:04 ", Desc: "Kyle Murphy picked up the win via pinfall and punches his ticket to join the Shock Rock Rumble", Rating: 3.5 },
@@ -39,11 +40,13 @@ export const provingGroundsData = [
 { Header: "Alex Dean vs Aiden Travis - 1:03:22", Desc: "Alex Dean punishes his former tag team partner in a thrilling steel cage match and walks out with the win after pinning Aiden Travis.", Rating: 4.25 },
 { Header: "Jacob vs Caleb Blaze vs Vinny Diaz vs Russell Tanaka - Proving Grounds Championship - 1:14:17", Desc: "After Jacob is eliminated first we are guaranteed a new Proving Grounds Champion. Russell Tanaka manages to avoid falling victim to the team of Blaze & Diaz and becomes the new Proving Grounds Champion! ", Rating: 4.5 },];
 export const strikeData = [
-    { Header: "Dawn Holloway vs Skull Galaxy - European Championship 4:46", Desc: "Skull retains the Women's European Championship via pinfall. ", Rating: 3 },
-{ Header: "Samoan Supers vs Damian Cruz & Russell Tanaka - 14:23", Desc: "Cruz & Tanaka pick up a very impressive victory with Russell getting the pinfall. ", Rating: 3.5 },
-{ Header: "Skywalker vs Jay Wolf - 21:38", Desc: "Skywalker defeats the Community Creations Champion Jay Wolf in a non title match after hitting an impressive series of kicks.", Rating: 3.5 },
-{ Header: "Inferno Jonny vs Cameron Jones - 30:45", Desc: "Cameron Jones picks up the win debuting his new persona. ", Rating: 3 },
-{ Header: "Golden Bucks vs Vengeance - 37:10", Desc: "Vengeance steals a win as it appears that the referee missed a Ricky Star kick out. ", Rating: 2 },
-{ Header: "TJ Baker & Eoin vs Kevin Page & Kyle Murphy - 42:23", Desc: "Eoin is TJs mystery partner staring down Ace on his way out to the ring. Eoin & TJ win a chaotic tag match after TJ rolls up Kyle Murphy", Rating: 4.5 },
-{ Header: "Arik Alexander vs Emerson Rivers vs Judgement vs Zach Gold - #1 Contender for Shockwave Global Championship - 1:00:25", Desc: "Emerson Rivers becomes the new #1 Contender after hitting a 5 Petal Frog Splash on Judgement and getting the pin. ", Rating: 4.5 },
+    { Header: "Rodkip vs Lucario, Squirtle and Gengar - 3-on-1 Handicap Match - 0:43", Desc: "Rodkip takes yet another loss as he falls victim to the numbers advantage losing to JVHs hand picked Pokemon team.", Rating: 3 },
+{ Header: "Spencer Saints vs Eoin - European championship and Pokemon Master championship - 9:26", Desc: "Eoin answered Spencer Saints open challenge and gains yet another win on his impressive undefeated streak while Spencer falls to 0-10.", Rating: 3.5 },
+{ Header: "Evan Stark vs Luke Mercer - 19:04", Desc: "Luke Mercer picked up a victory via pinfall in a great back and forth between the two rookies. ", Rating: 3.5 },
+{ Header: "Vengeance vs Russell Tanaka & Damian Cruz - 29:02", Desc: "Damian & Russell cruise to a victory picking up their 2nd straight win as a tag team.", Rating: 3.5 },
+{ Header: "Vittorio Grau vs Skywalker - 38:17", Desc: "Skywalker gets the win over Vittorio Grau and will be the new #1 contender for the Community Creations Championship ", Rating: 3.5 },
+{ Header: "TJ Baker vs 'Protostar' Kyle Murphy - 46:28", Desc: "TJ Baker kept his momentum going picking up a victory via pinfall. Post Match it looked like Kyle Murphy was gearing up to attack TJ but Big H made the save. ", Rating: 4 },
+{ Header: "Flyboy DJ Holmes vs Max Marshall - 55:03", Desc: "Max Marshall picks up a great win in a tough match against DJ Holmes. ", Rating: 4 },
+{ Header: "Zach Gold (c) vs Arik Alexander - SWF World Championship - 1:07:17", Desc: "Zach Gold was able to retain the SWF World Championship after getting an assist from Ricky Star", Rating: 3.5 },
+{ Header: "Delilah Sheilds vs SkullGalaxy - 1:17:15", Desc: "Skull retains the Women's European Championship and earns herself a shot at the Belt of Artemis. ", Rating: 4.5 },
 ];
